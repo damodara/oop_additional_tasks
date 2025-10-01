0f22356e11109f540aa6d996d3e3501a62eda276
damodara <jay108@mail.ru>
2025-10-02 00:22:24 +0300

    lesson 1 - albums.py - done

--- lesson1/albums.py
+++ lesson1/albums.py
@@ -21,15 +21,19 @@
 """
 
 
-class ...:
+class Album:
+    artist: str
+    title: str
+    tracks: list
+    def __init__(self, artist, title, tracks):
+        self.artist = artist
+        self.title = title
+        self.tracks = tracks
 
-    def ...:
-    ...
 
+album_1 = Album("Queen", "Killer Queen", ["Brighton rock", "Killer Queen", "Tenement Funster"])
 
-album_1 = Album(...)
-
-album_2 = ...
+album_2 = Album("Metallica", "Black Album",["Enter Sandman", "Sad But True", "Holier Than Thou"])
 
 
 # код для проверки 
