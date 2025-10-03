52be683069dc7b64d6d1cf878f78df6a769598a1
damodara <jay108@mail.ru>
2025-10-03 23:48:09 +0300

    lesson 2 - user.py - done

--- lesson2/user.py
+++ lesson2/user.py
@@ -14,10 +14,56 @@
 
 
 class User:
-    pass
+    def __init__(self, name, password):
+        self._name = name
+        self._password = password
+        self._is_admin = False
+        self._is_logged_in = False
 
 
-# код для проверки 
+    @property
+    def name(self):
+        """
+        Возвращает имя пользователя.
+        """
+        return self._name
+
+
+    @property
+    def password(self):
+        return self._password
+
+
+    @password.setter
+    def password(self, new_password):
+        if len(new_password) > 5:
+            self._password = new_password
+        else:
+            raise ValueError("Пароль должен содержать минимум 6 символов и хотя бы одну цифру.")
+
+
+    @property
+    def is_admin(self):
+        return self._is_admin
+
+
+    def login(self, password):
+        if password == self.password:
+            self._is_logged_in = True
+            return True
+        else:
+            return False
+
+
+    def logout(self):
+        if self._is_logged_in:
+            self._is_logged_in = False
+            return True
+        else:
+            return False
+
+
+# код для проверки
 user1 = User("Alice", "qwerty")
 print(user1.name)  # Alice
 print(user1.password)  # qwerty
