04bff9d1cc1e912691fd057a3188781f1e0d3306
damodara <jay108@mail.ru>
2025-10-04 13:28:27 +0300

    lesson 3 - mylist.py - done

--- lesson3/mylist.py
+++ lesson3/mylist.py
@@ -11,8 +11,24 @@
 
 
 class MyList:
-    pass
+    def __init__(self, data):
+        self.data = data
 
+
+    def __repr__(self):
+        return f'MyList({self.data})'
+
+
+    def __str__(self):
+        return f'{self.data}'
+
+
+    def __len__(self):
+        return len(self.data)
+
+
+    def __add__(self, other):
+        return MyList(self.data + other.data)
 
 # код для проверки 
 my_list1 = MyList([1, 2, 3])
