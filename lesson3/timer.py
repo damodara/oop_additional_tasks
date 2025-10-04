5fc68eb675f6507bc1a8b25b39e5c24b57444c5c
damodara <jay108@mail.ru>
2025-10-04 13:48:00 +0300

    lesson 3 - timer.py - done

--- lesson3/timer.py
+++ lesson3/timer.py
@@ -8,11 +8,23 @@
 
 
 class Timer:
-    pass
+    def __enter__(self):
+        self.count_start = 0
+        self.elapsed_time = None
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        count_end = 0
+        for _ in range(1000000):
+            count_end += 1
+        self.elapsed_time = count_end - self.count_start
+        print(f"Приблизительное время выполнения: {self.elapsed_time / 1000000 : .6f} условных единиц.")
 
 
 with Timer() as timer:
     # блок кода
-    
+    total_sum = 0
+    for i in range(1_000_000):
+        total_sum += i
     # код для проверки 
     print("Execution time:", timer.elapsed_time)
